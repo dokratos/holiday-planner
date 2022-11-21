ef75dd955741d3d85f195c0ba1174f28c316b683
salt-dev <salt-dev@salt-devs-MacBook-Air-9.local>
2022-11-21 11:51:39 +0100

    set up heroku

--- app.js
+++ app.js
@@ -13,9 +13,9 @@ app.use(cors());
 app.use(express.json());
 
 app.use(express.static(path.resolve(__dirname, './client/build')));
-// app.get('*', (req, res) => {
-//   res.sendFile(path.resolve(__dirname, './client/build', 'index.html'));
-// });
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, './client/build', 'index.html'));
+});
 
 app.get('/', async (req, res) => {
   const data = await get();
