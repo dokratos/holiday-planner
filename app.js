ac331c1a38ee779b697123d01a0dab7fc4b265d7
salt-dev <salt-dev@salt-devs-MacBook-Air-9.local>
2022-11-24 09:44:52 +0100

    Fix dynamic routing

--- app.js
+++ app.js
@@ -13,12 +13,13 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use(express.static(path.resolve(__dirname, './client/build')));
 
 if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.resolve(__dirname, './client/build')));
+
   app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, "/client/public/index.html"));
-	  app.use(express.static('client/build'));
+    res.sendFile(path.join(__dirname, '/client/build/index.html'));
+	  // app.use(express.static('client/build'));
   });
 }
 
